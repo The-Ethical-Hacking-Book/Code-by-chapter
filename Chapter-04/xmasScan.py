931d92e657e6528775dd04e5bb969a70e1c96bf9
Daniel <dgg6b@virginia.edu>
2022-02-09 19:54:57 -0500

    add xmas scan solution

---
+++ Chapter-04/xmasScan.py
@@ -0,0 +1,8 @@
+from scapy.all import *
+
+def processPacket(packet):
+	if packet['TCP'].flags ==  0x29:
+		print("Xmas Scan Detected on Port ",packet['TCP'].dport)
+
+sniff(iface="lo", count = 1000, filter="tcp", store=0, prn = processPacket)
+
